733cfff92b77c1b4e6d2c29c5eae70322f04f444
Jcdcm20 <jcdcm20@gmail.com>
2022-12-23 14:50:14 -0300

    Fix bug where all row changed color

--- scripts.js
+++ scripts.js
@@ -1,10 +1,7 @@
-const button = document.querySelector('button');
-const container = document.getElementById('container');
-
 function createSketchPad(numSquares) {
+    const container = document.getElementById('container');
     for (let i = 1; i <= numSquares; i++) {
         let column = document.createElement('div');
-        column.classList.add('square');
         column.classList.add('column');
         for (let j = 1; j <= numSquares; j++) {
             let row = document.createElement('div');
@@ -13,19 +10,22 @@ function createSketchPad(numSquares) {
         }
         container.appendChild(column);
     }
+
+    const squares = document.querySelectorAll('.square');
+
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = '#000000';
+        })
+    })
 }
+
 
 createSketchPad(16);
 
+const button = document.querySelector('button');
 button.addEventListener('click', () => {
+    
     let numSquares = parseInt(prompt("How many squares in the Sketch Pad"));
     createSketchPad(numSquares);
-})
-
-const squares = document.querySelectorAll('.square');
-
-squares.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = '#eeeeee';
-    })
 })
